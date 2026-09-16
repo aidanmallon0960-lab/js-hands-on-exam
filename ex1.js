e6d81c397aaccde77163d856751e8e24a94133a3
aidan <aidanmallon09@gmail.com>
2026-09-16 13:07:00 -0400

    initial commit

---
+++ ex1.js
@@ -0,0 +1,21 @@
+let price;
+let quantity;
+let total;
+let subtotal;
+let discount = 0;
+const calcBtn = document.getElementById("calculate");
+const priceres = document.getElementById("priceres");
+const subtotalres = document.getElementById("subtotal");
+
+calcBtn.addEventListener("click", function () {
+  price = Number(document.getElementById("price").value);
+  quantity = Number(document.getElementById("quantity").value);
+  total = price*quantity
+  subtotal = total
+  if (total >= 50){
+    discount = total *.1
+    total = total - discount
+  }
+  priceres.innerHTML = `Price: $${price} and quantity: ${quantity}`
+  subtotalres.innerHTML = `Subtotal: $${subtotal.toFixed(2)}  Discount: $${discount.toFixed(2)}  Final total: $${total.toFixed(2)}`
+});
